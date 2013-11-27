32283441e03ddeccc702a2cc4493fdcc97f30437
Neel Smith <dnsmith.neel@gmail.com>
2013-11-27 12:43:33 -0500

    Expand unit test on prev-next.

--- src/test/groovy/edu/holycross/shot/chron/TestPrevNext.groovy
+++ src/test/groovy/edu/holycross/shot/chron/TestPrevNext.groovy
@@ -9,11 +9,11 @@ class TestPrevNext extends GroovyTestCase {
     String sparql = "http://localhost:3030/ds/"
 
 
-    String first = "urn:cite:chron:heraclian.1"
-    String ruler = "urn:cite:chron:heraclian.2"
-    String nextFull = "urn:cite:chron:heraclian.3"
 
-    void testSynchronisms() {
+    void testFromCollection() {
+        String first = "urn:cite:chron:heraclian.1"
+        String ruler = "urn:cite:chron:heraclian.2"
+        String nextFull = "urn:cite:chron:heraclian.3"
         JGraph jg = new JGraph(sparql)
         def fullPN =  jg.getPrevNext(ruler)
         assert fullPN[0] == first
@@ -22,5 +22,18 @@ class TestPrevNext extends GroovyTestCase {
         def firstPN = jg.getPrevNext(first)
         assert firstPN[0] == ""
      }
+
+
+    void testOlympiad() {
+        JGraph jg = new JGraph(sparql)
+        String olympiad = "urn:cite:chron:olympiadyear.24_1"
+        String expectedPrev = "urn:cite:chron:olympiadyear.92"
+        String expectedNext = "urn:cite:chron:olympiadyear.94"
+
+        def olyPN =  jg.getPrevNext(olympiad)
+        assert olyPN[0] == expectedPrev
+        assert olyPN[1] == expectedNext
+    }
+
 
 }
